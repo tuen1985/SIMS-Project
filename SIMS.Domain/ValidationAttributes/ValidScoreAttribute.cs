d93ac70c32f0b029702855508fedd902d500eb62
nguyenducdong22 <dong160805@gmail.com>
2025-08-14 15:18:25 +0700

    sửa phần anh Hưng

---
+++ SIMS.Domain/ValidationAttributes/ValidScoreAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using SIMS.Domain;
+
+namespace SIMS.Domain.ValidationAttributes
+{
+    public class ValidScoreAttribute : ValidationAttribute
+    {
+        private readonly float _minScore;
+        private readonly float _maxScore;
+
+        public ValidScoreAttribute(float minScore, float maxScore)
+        {
+            _minScore = minScore;
+            _maxScore = maxScore;
+            ErrorMessage = $"The score must be between {_minScore} and {_maxScore}.";
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is float score)
+            {
+                if (score < _minScore || score > _maxScore)
+                {
+                    return new ValidationResult(ErrorMessage);
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
